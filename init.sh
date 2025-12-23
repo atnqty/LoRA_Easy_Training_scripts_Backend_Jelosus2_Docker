e7782c8577faac6d76accb2f8ce975a68b268e35
atnqty <49927384+atnqty@users.noreply.github.com>
2025-12-23 16:14:23 -0500

    Refactor init.sh to support multiple forks

--- init.sh
+++ init.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 echo "Starting Stable Diffusion Trainer"
-if [ ! -d "/trainer" ] || [ ! "$(ls -A "/trainer")" ]; then
+
+FORK=${FORK:-derrian-distro}
+
+if [ ! -d "/trainer/$FORK" ] || [ ! "$(ls -A "/trainer/$FORK")" ]; then
   echo "Files not found, cloning..."
-  mkdir -p /trainer
-  if [ "$FORK" = "jelosus" ]; then
-    echo "Using Jelosus2's fork"
-    git clone https://github.com/Jelosus2/LoRA_Easy_Training_scripts_Backend.git /trainer
-  else
-    echo "Using derrian-distro"
-    git clone https://github.com/derrian-distro/LoRA_Easy_Training_scripts_Backend.git /trainer
-  fi
-  cd /trainer
-  python3.10 /trainer/installer.py local
-  chmod +x /trainer/run.sh
-  exec /trainer/run.sh $ARGS
+  mkdir -p /trainer/$FORK
+  git clone https://github.com/$FORK/LoRA_Easy_Training_scripts_Backend.git /trainer/$FORK
+  cd /trainer/$FORK
+  python3.10 /trainer/$FORK/installer.py local
+  chmod +x /trainer/$FORK/run.sh
+  exec /trainer/$FORK/run.sh $ARGS
 else
   echo "Files found, starting..."
-  cd /trainer
+  cd /trainer/$FORK
   git pull
-  chmod +x /trainer/run.sh
-  exec /trainer/run.sh $ARGS
+  chmod +x /trainer/$FORK/run.sh
+  exec /trainer/$FORK/run.sh $ARGS
 fi
